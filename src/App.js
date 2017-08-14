6059480db0e5087731f9b9cc1eb23029ad766ca1
Anthony Rouseau <anthonyrouseau@Anthonys-MacBook-Pro.local>
2017-08-14 01:20:45 -0400

    page layout

--- src/App.js
+++ src/App.js
@@ -1,18 +1,14 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import MainView from './Components/MainView/MainView.js';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className="app-layout">
+        <div id="header">1</div>
+        <MainView id="main-view"/>
+        <div id="side-nav">3</div>
       </div>
     );
   }
---
+++ src/Components/MainView/MainView.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import './MainView.css';
+
+class MainView extends Component {
+  render() {
+    return(
+      <div id="layout">
+      </div>
+    );
+  }
+}
+
+export default MainView
